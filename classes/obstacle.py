5beedd83516f0059b51a8f1a747f3bf37f3e1006
jaime7981 <jmfernandez1@miaundes.cl>
2023-10-02 18:25:28 -0300

    obstacle class

--- classes/obstacle.py
+++ classes/obstacle.py
@@ -1,15 +1,30 @@
+from .Player import Player
+import random
 
-class Obstacle():
-    def __init__(self, width, height, position  = (0, 0)) -> None:
-        self.width = width
-        self.height = height
+class Obstacle:
+    def __init__(self):
+        self.difficulty = ""
+        self.size = 0
+        self.position = 0
 
-        self.position = position
+    def setDifficulty(self, difficulty):
+        if difficulty == "none":
+            self.difficulty = "none"
+            self.size = random.randint(0, 0.25)
+        if difficulty == "easy":
+            self.difficulty = "easy"
+            self.size = random.randint(0.26, 0.5)
+        if difficulty == "medium":
+            self.difficulty = "medium"
+            self.size = random.randint(0.51, 1)
+        if difficulty == "hard":
+            self.difficulty = "hard"
+            self.size = random.randint(1.1, 1.5)
+        
+    def setPosition(self, player1: Player, player2: Player):
+        if player1.position > player2.position:
+            self.position = (player1.position - player2.position)/2
+        if player2.position > player1.position:
+            self.position = (player2.position - player1.position)/2
 
-
-    def set_position(self, position) -> None:
-        self.position = position
-
-
-    def __str__(self) -> str:
-        return f'position: {self.position}, width: {self.width}, height: {self.height}'
+    
