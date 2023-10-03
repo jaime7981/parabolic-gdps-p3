441c5ed69cce3c11bc80321056273eb8fe3ec835
gustavolobosa <galobos@miuandes.cl>
2023-10-03 09:34:51 -0300

    initial setup

--- main.py
+++ main.py
@@ -1,11 +1,36 @@
 from classes.Enviroment import Enviroment
 from classes.Player import Player
+from classes.enums import Difficulty
+
+def validate_input(input):
+    if input == '0' or input == '1' or input == '2' or input == '3':
+        return True
+    else:
+        return False
+    
+def initial_setup():
+    
+    while True:
+        print("\nNone -> 0\nEasy -> 1\nMedium -> 2\nHard -> 3\n")
+        obstacle_diff = input('Enter obstacle difficulty [0-3]: ')
+        wind_diff = input('Enter wind difficulty [0-3]: ')
+        
+        if validate_input(obstacle_diff) and validate_input(wind_diff):
+            obstacle_diff = int(obstacle_diff)
+            wind_diff = int(wind_diff)
+            return obstacle_diff, wind_diff
+        
+        else:
+            print('Invalid input!\n')
+            continue
 
 def main():
     player_1 = Player(player_name = 'player_1')
     player_2 = Player(player_name = 'player_2')
+    
+    obstacle_diff, wind_diff = initial_setup()
 
-    game = Enviroment()
+    game = Enviroment(obstacle_diff = obstacle_diff, wind_diff = wind_diff)
 
     game.add_player(player_1)
     game.add_player(player_2)
